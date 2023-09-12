859843c0dcdc032605904882bcd26e8b38dd9f8e
Anurag upadhyay <80309151+Anurag319@users.noreply.github.com>
2023-09-12 14:13:57 +0530

    Time: 55 ms (92.50%), Space: 43.6 MB (96.25%) - LeetHub

--- 1224-maximum-equal-frequency/1224-maximum-equal-frequency.cpp
+++ 1224-maximum-equal-frequency/1224-maximum-equal-frequency.cpp
@@ -17,35 +17,29 @@ public:
         }
 
         for(int i=n-1;i>=0;i--){
-            // for(auto c : mp){
-            //     cout<<"mp"<<c.first<<" ss "<<c.second<<endl;
-            // }
-            // for(auto c : freq){
-            //     cout<<" freq "<<c.first<<" ss "<<c.second<<endl;
-            // }
-            // cout<<"---------------"<<endl;
+
             if(freq.size()==1){
                 for(auto c : freq){
-                    int p = c.second;
-                    int q = c.first;
+                    
+                    int p = c.second; // [4,4,4,4,4,4]  {6,1}
+                    int q = c.first;  // [3,4,5,6,7,8]  {1,6}
                     if(p==1 || q==1) return i+1;
                 }
             }
             else if(freq.size()==2){
+                
                 if(freq.find(1) != freq.end()){
                     if(freq[1]==1) return i+1;
                 }
                 auto a = freq.begin();
                 auto b = freq.rbegin();
                 
+                // [2,2,2,3,3,3,4,4,4,4] || [4,5,5,5,6,6,6]
                 if((b->first)-(a->first)==1 && (b->second)==1) return i+1;
             }
-            // for(auto c : freq){
-            //     cout<<" freq "<<c.first<<" ss "<<c.second<<endl;
-            // }
+
             if(freq[mp[nums[i]]]==1)freq.erase(mp[nums[i]]);
             else{
-                // cout<<"r "<<endl;
                 freq[mp[nums[i]]]--;
             }
             
@@ -54,17 +48,9 @@ public:
                 mp.erase(nums[i]);
             }
             else{
-                // cout<<" h "<<endl;
                 freq[mp[nums[i]]]++;
             }
             
-            // for(auto c : mp){
-            //     cout<<"mp"<<c.first<<" ss "<<c.second<<endl;
-            // }
-            // for(auto c : freq){
-            //     cout<<" freq "<<c.first<<" ss "<<c.second<<endl;
-            // }
-            // cout<<endl<<endl;
         }
         
         return 2;
